043826c4298a0c1d11a260af0bf1eca510e0b432
GustavoValdesZ <gustavovaldes.z@gmail.com>
2026-01-27 12:12:53 -0300

    se ajusta models.py y admin.py

--- cuestionario/admin.py
+++ cuestionario/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import (
+    Dimension,
+    NivelJerarquico,
+    Trabajador,
+    Competencia,
+    TextosEvaluacion,
+    Autoevaluacion,
+    EvaluacionJefatura
+)
 
-# Register your models here.
+admin.site.register(Dimension)
+admin.site.register(NivelJerarquico)
+admin.site.register(Trabajador)
+admin.site.register(Competencia)
+admin.site.register(TextosEvaluacion)
+admin.site.register(Autoevaluacion)
+admin.site.register(EvaluacionJefatura)
--- cuestionario/models.py
+++ cuestionario/models.py
@@ -1,3 +1,86 @@
 from django.db import models
 
-# Create your models here.
+class Dimension(models.Model):
+    id_dimension = models.IntegerField(primary_key=True)
+    nombre_dimension = models.CharField(max_length=30)
+
+    class Meta:
+        managed = False
+        db_table = 'dimension'
+
+
+class NivelJerarquico(models.Model):
+    id_nivel_jerarquico = models.IntegerField(primary_key=True)
+    nombre_nivel_jerarquico = models.CharField(max_length=30)
+
+    class Meta:
+        managed = False
+        db_table = 'nivel_jerarquico'
+
+
+class Trabajador(models.Model):
+    id_trabajador = models.IntegerField(primary_key=True)
+    rut = models.CharField(max_length=20)
+    nombre = models.CharField(max_length=40)
+    apellido_paterno = models.CharField(max_length=40)
+    apellido_materno = models.CharField(max_length=40)
+    email = models.CharField(max_length=80)
+    departamento = models.CharField(max_length=40)
+    cargo = models.CharField(max_length=40)
+    genero = models.CharField(max_length=10)
+    nivel_jerarquico_id_nivel_jerarquico = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'trabajador'
+
+
+class Competencia(models.Model):
+    id_competencia = models.IntegerField(primary_key=True)
+    nombre_competencia = models.CharField(max_length=30)
+    dimension_id_dimension = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'competencia'
+
+
+class TextosEvaluacion(models.Model):
+    id_textos_evaluacion = models.IntegerField(primary_key=True)
+    codigo_excel = models.CharField(max_length=10, unique=True)
+    texto = models.TextField()
+    competencia_id_competencia = models.IntegerField()
+    nivel_jerarquico_id_nivel_jerarquico = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'textos_evaluacion'
+
+
+class Autoevaluacion(models.Model):
+    id_autoevaluacion = models.IntegerField(primary_key=True)
+    puntaje = models.DecimalField(max_digits=5, decimal_places=2)
+    fecha_evaluacion = models.DateField()
+    momento_evaluacion = models.DateTimeField()
+    comentario = models.TextField(null=True, blank=True)
+    trabajador_id_trabajador = models.IntegerField()
+    codigo_excel = models.CharField(max_length=10)
+
+    class Meta:
+        managed = False
+        db_table = 'autoevaluacion'
+
+
+class EvaluacionJefatura(models.Model):
+    id_evaluacion_jefatura = models.IntegerField(primary_key=True)
+    puntaje = models.DecimalField(max_digits=5, decimal_places=2)
+    evaluador = models.CharField(max_length=50)
+    fecha_evaluacion = models.DateField()
+    momento_evaluacion = models.DateTimeField()
+    comentario = models.TextField(null=True, blank=True)
+    trabajador_id_trabajador = models.IntegerField()
+    codigo_excel = models.CharField(max_length=10)
+
+    class Meta:
+        managed = False
+        db_table = 'evaluacion_jefatura'
